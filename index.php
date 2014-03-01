e852e39a5538c26ecf94b4d3fc03848ffa2a8500
Marlo Longley <marlo.e.longley@gmail.com>
2014-02-28 17:03:23 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,216 @@
+<?php include 'header.php' ?>
+<?php include 'modal.php' ?>
+
+<div class="bg0">
+    <div class="container">
+    
+    <!-- jumbo row -->
+      <div class="row jumbo" data-scroll-index="0">
+        <div class="col-md-10 col-md-offset-1">
+          <img src="img/jumbo.png" class="padding-bottom">
+          	<div class="col-md-7 col-md-offset-1">
+			  	<h3>We work on all kinds of projects, but we get really excited about the jobs that others shy from. Our passion and experience with Force.com makes us some of the most skilled programmers and app developers you'll find. Some might even say we make a science of it.</h3>
+          	</div>
+          	<div class="col-md-3">
+          		<a href="#"><img src="img/btn-learn.png"></a>
+          	</div>
+        </div> <!-- /cols -->
+      </div> <!-- /jumbo -->
+      
+      <!-- what row -->
+      <div class="row what" id="what" data-scroll-index="1">
+      	<div class="col-md-1">
+      		<h3>WHAT</h3>
+      	</div>
+      	<div class="col-md-10 col-md-offset-1">
+      		<h4 class="clean-top">What we do is Force.com platform development, AppExchange product development, and CRM implementation and business process consulting, but how we do it is way more interesting. We work on all levels of projects, but we enjoy especially challenging terrain. It’s important that our customers walk away with their expectations surpassed for a price that works. We also like to have some fun along the way.</h4>
+      		<div class="col-md-4">
+      			<div class="icon"><img src="img/what1.png"></div>
+      			<h4 class="bold">FORCE.COM PLATFORM DEVELOPMENT</h4>
+      			<p>At our core, we’re a Force.com development shop. We do custom platform development at any level of technical difficulty. Projects involving Apex, Visualforce, portals, and Sites are our specialty. Just define the requirements and we'll use our expertise to help you leverage the Force.com platform quickly and powerfully.</p>
+      			<a href="successes"><div class="icon"><img src="img/btn-success.png"></div></a>
+      		</div>
+      		<div class="col-md-4">
+      			<div class="icon"><img src="img/what2.png"></div>
+      			<h4 class="bold">APPEXCHANGE PRODUCT DEVELOPMENT</h4>
+      			<p>We use our Force.com domain knowledge and technical expertise to develop applications for companies to deploy on the AppExchange. Whether your product adds functionality to Salesforce, integrates your existing product with the Force.com platform, or just does something cool, we want to build it.</p>
+	  			<a href="successes"><div class="icon"><img src="img/btn-success.png"></div></a>
+      		</div>
+      		<div class="col-md-4">
+      			<div class="icon"><img src="img/what3.png"></div>
+      			<h4 class="bold">CRM IMPLEMENTATION & BUSINESS PROCESS CONSULTING</h4>
+      			<p>Our skilled project managers can set up sales, service, and other business processes for a diverse range of companies, both in size and offering. From basic implementations to complex projects extending Salesforce.com’s CRM capabilities, we approach each project with creativity, efficiency, and a keen eye for detail.</p>
+      			<a href="successes"><div class="icon"><img src="img/btn-success.png"></div></a>
+	  		</div>
+      	</div> <!-- /cols -->
+      </div> <!-- /what -->
+      
+      <!-- successes row -->
+      <div class="row successes" data-scroll-index="2">
+	      <div class="col-md-12">
+	      	<h3>SUCCESSES</h3>
+	      </div>
+	      <div class="col-md-12">
+	      	<h4 class="clean-top">Nothing illustrates a company's ability quite like a story, so we'd like to share a few of how we put our science to the test.</h4>
+	      	<!-- begin carousel -->
+	      	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			  	<a href="successes"><div class="carousel-inner">
+			  	<!-- constant contact -->
+				  	<div class="item active">
+						  	<div class="col-md-6">
+							  	<div class="inner-img">
+							  		<img src="img/slide-1-1.jpg">
+							  		<img src="img/slide-1-2.jpg">
+							  	</div>
+							  	<p>Constant Contact provides online marketing services to small businesses and nonprofits.</p>
+							  	<h5>CHALLENGE</h5>
+							  	<p>Build a mass market integration between Salesforce and Constant Contact.</p>
+							  	<h5>SOLUTION</h5>
+							  	<p>And AppExchange product that can be used by Salesforce organizations to upload contacts and leads from Salesforce to Constant Contact, plus keep opt-out data synchronized between systems.</p>
+						  	</div>
+						  	<div class="col-md-6">
+						  		<img src="img/slide-1-3.png">
+						  	</div>
+					</div> <!-- /item -->
+					<!-- greenway -->
+					<div class="item">
+						<div class="col-md-6">
+							  	<div class="inner-img">
+							  		<img src="img/slide-2-1.png">
+							  	</div>
+							  	<p>Greenway provides innovative information solutions to improve healthcare.</p>
+							  	<h5>CHALLENGE</h5>
+							  	<p>Greenway Medical Technologies wanted to enable their customers to allow patients access to their own healthcare records through a Patient Portal that spoke to both the needs of the patients and providers within HIPAA guidelines.</p>
+							  	<h5>SOLUTION</h5>
+							  	<p>A Force.com embedded product that integrated Greenway's core product offering, PrimeSUITE, to increase data integrity and patient confidence.</p>
+						  	</div>
+						  	<div class="col-md-6">
+						  		<img src="img/slide-2-2.png">
+						  	</div>
+						</div> <!-- /item -->
+					<!-- health market science -->
+					<div class="item">
+						<div class="col-md-6">
+							  <div class="inner-img">
+									<img src="img/slide-3-1.png">
+							  </div>
+							  <p>A leading supplier of healthcare provider data and end-to-end solutions that address data management, regulatory compliance and market intelligence.</p>
+							  <h5>CHALLENGE</h5>
+							  <p>Give the healthcare market an easier, faster way to access healthcare practitioner data through the Salesforce platform.</p>
+							  <h5>SOLUTION</h5>
+							  <p>An AppExchange product, Provider MasterFile, which can be installed into Salesforce orgs.</p>
+						  	</div>
+						  	<div class="col-md-6">
+						  		<img src="img/slide-3-2.png">
+						  	</div>
+						</div> <!-- /item -->
+						<!-- glance -->
+					<div class="item">
+						<div class="col-md-5">
+							  <div class="inner-img">
+									<img src="img/slide-4-1.png">
+							  </div>
+							  <p>Glance enables sales and support reps to launch demos, presentations and support sessions directly or from within applications they're already using - like Salesforce.com and LivePerson.</p>
+							  <h5>CHALLENGE</h5>
+							  <p>Glance Networks, the #7 screen sharing provider, wanted a Salesforce-integrated technological edge to help sales pros make the greatest impact on every call throughout the sales cycle.</p>
+							  <h5>SOLUTION</h5>
+							  <p>A composition application that integrates with native page layouts, Glance API, and Chatter.</p>
+						  	</div>
+						  	<div class="col-md-3 top">
+						  		<img src="img/slide-4-2.png">
+						  	</div>
+						  	<div class="col-md-4">
+						  		<p>Users can view each other's screen instantly from records in Salesforce</p>
+						  		<img class="clean" src="img/slide-4-3.png">
+						  		<p>Schedule future sessions from Accounts, Contacts, Leads, Cases, and Campaigns</p>
+						  	</div>
+						</div> <!-- /item -->
+						<div class="item">
+							<div class="col-md-4">
+							  <div class="inner-img">
+							  	<p>We're proud of our many clients and the solutions we've developed together.</p>
+							  	<img src="img/slide-5-1.png">
+							  </div>
+							 </div>
+						  	<div class="col-md-6">
+							  	<img src="img/slide-5-2.png">
+						  	</div>
+						</div> <!-- /item -->
+					</div> <!-- /carousel-inner -->
+					<div class="carousel-nav-container">
+					<ul class="carousel-nav">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"><img src="img/carousel-nav-0.png"></li>
+						<li data-target="#myCarousel" data-slide-to="1"><img src="img/carousel-nav-1.png"></li>
+						<li data-target="#myCarousel" data-slide-to="2"><img src="img/carousel-nav-2.png"></li>
+						<li data-target="#myCarousel" data-slide-to="3"><img src="img/carousel-nav-3.png"></li>
+						<li data-target="#myCarousel" data-slide-to="4"><img src="img/carousel-nav-4.png"></li>
+					</ul>
+					</div>
+				</div></a><!-- /.carousel -->
+			</div> <!-- /cols -->
+		</div> <!-- /successes -->
+    </div> <!-- /container -->
+</div> <!-- /bg0 -->
+
+<div class="bg1">
+	<div class="container">
+<!-- Example row of columns -->
+      <div class="row who" data-scroll-index="3">
+        <div class="col-md-1">
+      		<h3>WHO</h3>
+      	</div>
+        <div class="col-md-10 col-md-offset-1">
+      		<div class="col-md-4">
+      			<div class="icon"><img src="img/who-1.jpg"></div>
+      			<h4 class="bold underline">MIKE WITHERSPOON</h4>
+      			<h4 class="bold">SAN FRANCISCO, CA</h4>
+      			<h4 class="italic">SOLUTION ARCHITECT & COFOUNDER</h4>
+      			<p>Mike is a certified Force.com consultant who loves tackling really hard problems with innovative solutions. After twelve years of improving business processes for a broad client list, Mike likes to say that he's earned one of the most practical MBAs in the world.</p>
+      		</div>
+      		<div class="col-md-4">
+      			<div class="icon"><img src="img/who-2.jpg"></div>
+      			<h4 class="bold underline">TIM YOUNGBLOOOD</h4>
+      			<h4 class="bold">CHATTANOOGA, TN</h4>
+      			<h4 class="italic">TECHNICAL ARCHITECT & COFOUNDER</h4>
+      			<p>Tim has expertise in just about everything that warrants strings of indecipherable capital letters. He’s a technologist, developer, and entrepreneur who began programming at the age of 12.</p>
+      		</div>
+      		<div class="col-md-4">
+      			<div class="icon"><img src="img/who-3.jpg"></div>
+      			<h4 class="bold underline">BRIAN WALSH</h4>
+      			<h4 class="bold">SAN FRANCISCO, CA</h4>
+      			<h4 class="italic">SOLUTION ARCHITECT & PARTNER</h4>
+      			<p>For the past 14 years, Brian has been building SaaS products across a wide range of enterprise and consumer offerings. He believes you don't have to be the smartest person in the room to solve the most difficult problems; you have to ask the smartest questions.</p>
+	  		</div>
+      	</div> <!-- /cols -->
+      	<div class="col-md-12 center">
+      		<h4><a href="team">meet the whole team</a></h4>
+      </div> <!-- /row -->
+	</div>
+</div>
+</div> 
+
+<div class="bg2">
+	<div class="container">
+      <!-- Example row of columns -->
+      <div class="row jobs" data-scroll-index="4">
+        <div class="col-md-1">
+        	<h3>JOBS</h3>
+		</div>
+        <div class="col-md-8 col-md-offset-1">
+          <h4>SEEKING MAD SCIENTISTS</h4>
+          <p>Part of our culture at CodeScience is to treat everybody like an adult. This is handy because, while kids are cool, we don’t hire them. Instead, our team has a ton of experience building solutions and products on the Force.com platform (among others) and includes previous Salesforce.com employees and instructors. We counter technical expertise with creative thinking and a need to push boundaries and ourselves. We're committed to doing the job right and having a blast while doing it. Sound like you? 
+Let’s talk.</p>
+		  <p><a href="jobs#appexch">APPEXCHANGE PRODUCT MANAGER</a></p>
+		  <p><a href="jobs#cloudapp">CLOUD APPLICATION DEVELOPER</a></p>
+		  <p>PROJECT MANAGER</p>
+	      <p> SOLUTION ARCHITECT</p>
+		  <p>RECRUITER OF ALL TRADES</p>
+       </div>
+        <div class="col-md-1">
+           <a href="jobs"><div class="icon"><img src="img/btn-learn-sm.png"></div></a>
+        </div>
+      </div> <!-- /row -->
+	</div>
+</div>
+
+<?php include 'footer.php' ?>
